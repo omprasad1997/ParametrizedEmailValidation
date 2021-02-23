ba5b2797ccc63d6bf976cabf8783d0d217708441
Omprasad Rathod <20rathodomprasad@gmail.com>
2021-02-23 23:47:26 +0530

    First Commit

---
+++ main/java/com/emailparameterised/UserValidator.java
@@ -0,0 +1,12 @@
+package com.emailparameterised;
+
+import java.util.regex.Pattern;
+
+public class UserValidator {
+    private static final String EMAIL_ADDRESS = "^(abc)?(.+)@([0-9a-zA-Z][-]?)([.][a-z]{2,3})?(.*)$";
+
+    public boolean validEmailAddress(String email) {
+        return Pattern.matches(EMAIL_ADDRESS, email);
+    }
+    
+}
---
+++ test/java/com/emailparameterised/ValidateEmailTest.java
@@ -0,0 +1,38 @@
+package com.emailparameterised;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import java.util.Arrays;
+import java.util.Collection;
+
+    @RunWith(Parameterized.class)
+    public class ValidateEmailTest {
+
+        private String emailTest;
+        private boolean expectedResult;
+
+        public ValidateEmailTest(String email,boolean expectedResult) {
+            this.emailTest = email;
+            this.expectedResult = expectedResult;
+        }
+
+    @Parameterized.Parameters
+    public static Collection data(){
+            return Arrays.asList(new Object[][] { {"abc@yahoo.com",true},
+                                                  {"abc-100@yahoo.com.dasd",false},
+                                                  {"abc111@yahoo.com",true},
+                                                  {"abc-100@abc.com",true},
+                                                  {"abc.100@abc.com",true},
+                                                  {"abc+100@gmail.com",true},
+            });
+    }
+    @Test
+    public void givenEmailAsVar_ShouldReturnAsPerParametrisedResult{
+        UserValidator validator = new UserValidator();
+        boolean result = validator.validEmailAddress(this.emailTest);
+        Assert.assertEquals(this.expectedResult,result);
+    }
+}
+
